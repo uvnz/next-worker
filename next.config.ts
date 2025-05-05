1dce10f86e68a39b05aae767d2dea0210a48ceb1
uvnz <devn_@outlook.com>
2025-05-05 09:28:22 +0800

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   allowedDevOrigins: ["txl9qt-3000.csb.app"],
+  output: 'export',
+  distDir: 'dist',
 };
 
 export default nextConfig;
